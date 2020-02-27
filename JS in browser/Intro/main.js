4239ab3f572218149dd21aba3ebc722bcd9a244d
Milosm991 <milosmanojlovic91@gmail.com>
2020-02-27 23:07:27 +0100

    js-browser

---
+++ JS in browser/Intro/main.js
@@ -0,0 +1,87 @@
+// function sum(a, b) {
+//     if (a > b) {
+//         return true
+//     } else {
+//         return false
+//     }
+
+// }
+// var res = sum(6, 5);
+// console.log(res);
+
+
+
+// function printInfo(info) {
+//     console.log(info)
+// }
+
+// printInfo(window.navigator.platform)
+// printInfo(window.navigator.appVersion)
+// printInfo(window.navigator.vendor)
+
+
+// function printInfo(info) {
+//     console.log(info)
+// }
+
+
+// printInfo(window.screen.width);
+// printInfo(window.screen.height);
+// printInfo(window.screen.availHeight);
+
+
+// function print(info) {
+//     console.log(info);
+
+// }
+
+// print(window.location.href);
+// print(window.location.protocol);
+// print(window.location.host);
+// print(window.location.search);
+
+// function rel() {
+//     location.reload();
+// }
+
+
+// function changePage() {
+//     location.href = "https://google.com"
+// }
+
+// function hist() {
+//     history.back
+// }
+
+// function str(a, b) {
+//     localStorage.setItem(a, b);
+// }
+
+// str("a", "Nikola");
+
+// function readData() {
+//     var name = localStorage.getItem("name");
+
+//     if (name) {
+//         console.log(name)
+//     } else {
+//         console.log("There is no data")
+//     }
+// }
+
+// readData();
+
+
+
+// function messages() {
+//     alert("Zdravo");
+//     var b = prompt("Da li si siguran?");
+//     if (b) {
+//         confirm("Tvoj odgovor je " + b)
+//     }
+
+// }
+// messages();
+
+
+
