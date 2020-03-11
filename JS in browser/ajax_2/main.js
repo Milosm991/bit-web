41d36bd37b4b8c62eec46dc1ed38f183d9b07245
BIT Student <classroom@bgit.rs>
2020-03-11 12:55:47 +0100

    gitsearcher

--- JS in browser/ajax_2/main.js
+++ JS in browser/ajax_2/main.js
@@ -107,23 +107,50 @@
 
 function handler() {
     var input = document.querySelector("input").value;
+    var main = document.querySelector("main")
+    var urlOne = "https://api.github.com/search/users?q="
+    var div = document.querySelector("div")
+
 
     var request = $.ajax({
-        url: "https://api.github.com/users?since=135",
-        method: "POST",
+        url: urlOne + input,
+        method: "GET",
         data: { id: 1 },
         dataType: "html"
     });
+    request.done(function (url) {
+        console.log(url)
+        var data = JSON.parse(url);
 
-    request.done(function (msg) {
-        $("#log").html(msg);
+        for (var i = 0; i < data.items.length; i++) {
+            var img = document.createElement("img")
+            var name = document.createElement("p")
+            var divv = document.createElement("div")
+            divv.className = "user"
+            img.setAttribute("src", data.items[i].avatar_url)
+            name.textContent = data.items[i].login
+            divv.appendChild(img)
+            divv.appendChild(name)
+            div.appendChild(divv)
+            main.appendChild(div)
+        }
     });
 
-    request.fail(function (jqXHR, textStatus) {
-        alert("Request failed: " + textStatus);
-    });
+    $(".user").on("click" = function (event) {
+        var user = event.currentTarget.querySelector('p').textContent
+        var request1 = new XMLHttpRequest();
+        request1.open("GET", "users/" + user + "/repos");
+        request1.send();
 
-    var search = document.querySelector("input")
-    search.addEventListener("click", handler)
+        request1.onload = function () {
+            var data = JSON.parse(request1.responseText);
+            var section = document.querySelector("section");
+        }
+
+    })
+
 
 }
+
+var search = document.querySelector("button")
+search.addEventListener("click", handler)
